b655bef4b2d34f8e2907f370cc47f747fe86437b
kasparmartens <kaspar.martens@gmail.com>
2016-11-26 16:06:38 +0000

    inference

---
+++ inference.R
@@ -0,0 +1,49 @@
+library(mvtnorm)
+
+matrix_list_sum = function(lst) Reduce("+", lst)
+
+infer_latent_factors = function(Y, X, k_latent, gamma, max_iter = 100){
+  K = k_latent
+  G = nrow(Y)
+  N = ncol(Y)
+  
+  # gamma = rep(0.1, G)
+  rho = 0.01
+  lambda = 1
+  # initialise Z
+  Z = matrix(0, G, K)
+  for(k in 1:K){
+    Z[, k] = rbinom(G, 1, 0.05)
+  }
+  # initialise W
+  W = matrix(rnorm(K*N), K, N)
+  # initialise beta
+  beta = rnorm(K, 0, 1)
+  
+  for(i in 1:max_iter){
+    # update Z
+    Z = update_Z(Y, Z, W, gamma, G, K, N)
+    
+    # update W
+    sigma_W_inv = lambda * diag(K) + matrix_list_sum(lapply(1:G, function(g){
+      gamma[g] * outer(Z[g, ], Z[g, ])
+    }))
+    sigma_W = solve(sigma_W_inv)
+    temp_W = lambda * beta * colSums(X) + matrix_list_sum(lapply(1:G, function(g){
+      gamma[g] * outer(Z[g, ], Y[g, ])
+    }))
+    mu_W = sigma_W %*% temp_W
+    noise = t(rmvnorm(N, rep(0, K), sigma_W))
+    W = mu_W + noise
+    
+    # update beta
+    x_colsums = colSums(X)
+    sigma_beta = 1 / (rho + sum(lambda * x_colsums**2))
+    mu_beta = sigma_beta * colSums(lambda * x_colsums * t(W))
+    beta = mu_beta + rnorm(K, 0, sqrt(sigma_beta))
+    
+    # update precision parameters
+    if(i>10) pheatmap(Z, cluster_cols=T, cluster_rows=T)
+  }
+  return(list(Z = Z, W = W, beta = beta))
+}
---
+++ inference.cpp
@@ -0,0 +1,59 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace Rcpp;
+
+arma::ivec colSums(const arma::imat & X){
+  int nCols = X.n_cols;
+  arma::ivec out(nCols);
+  for(int i = 0; i < nCols; i++){
+    out(i) = sum(X.col(i));
+  }
+  return(out);
+}
+
+// [[Rcpp::export]]
+double calculate_accept_prob(int g, int k, arma::imat& Z, arma::mat& W, arma::mat& Y, NumericVector gamma, int G, int K, int N, int count, double alpha){
+  arma::rowvec pred0(N), pred1(N);
+  // leave out latent factor k from prediction Y_pred(g, _)
+  if(Z(g, k) == 0){
+    pred0 = Z.row(g) * W;
+    pred1 = pred0 + W.row(k);
+  } else{
+    pred1 = Z.row(g) * W;
+    pred0 = pred1 - W.row(k);
+  }
+  arma::rowvec y = Y.row(g);
+  double loglik0 = sum(square(y-pred0)/gamma[g]);
+  double loglik1 = sum(square(y-pred1)/gamma[g]);
+  double z = log((count + alpha/K) / (G - count)) + loglik1 - loglik0;
+  double accept_prob = 1.0 / (1.0 + exp(-z));
+  return accept_prob;
+}
+
+// [[Rcpp::export]]
+arma::imat update_Z(arma::mat& Y, arma::imat Z, arma::mat & W, NumericVector gamma, int G, int K, int N) {
+  double alpha = 1.0;
+  //arma::imat Z(Z0.begin(), G, K, false);
+  arma::ivec genes_per_pathway = colSums(Z);
+  
+  //arma::mat Y_pred0 = Z * W;
+  for(int k=0; k<K; k++){
+    for(int g=0; g<G; g++){
+      // leave out gene g from the count
+      int count = genes_per_pathway[k] - Z(g, k);
+      double accept_prob =  calculate_accept_prob(g, k, Z, W, Y, gamma, G, K, N, count, alpha);
+      // set Z[g, k] to 1 with probability accept_prob
+      double u = R::runif(0, 1);
+      if(u < accept_prob){
+        Z(g, k) = 1;
+      } else{
+        Z(g, k) = 0;
+      }
+    }
+  }
+  return Z;
+}
+
+
+
