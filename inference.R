de219b197b044a165d53610becf3ee8882a376b2
kasparmartens <kaspar.martens@gmail.com>
2016-11-27 16:27:25 +0000

    updates for precision pars

--- inference.R
+++ inference.R
@@ -1,17 +1,27 @@
 library(mvtnorm)
+library(reshape2)
 
-matrix_list_sum = function(lst) Reduce("+", lst)
+source("helpers.R")
 
-infer_latent_factors = function(Y, X, k_latent, gamma, alpha, max_iter = 100, burnin = max_iter/2){
+infer_latent_factors = function(Y, X, k_latent, alpha, max_iter = 100, burnin = max_iter/2, keep_Y_trace = TRUE, which_genes = NULL, which_samples = NULL){
   K = k_latent
   G = nrow(Y)
   N = ncol(Y)
-  Ypred = matrix(0, nrow(Y), ncol(Y))
+  Ypred_mean = matrix(0, nrow(Y), ncol(Y))
   loglik = rep(0, max_iter - burnin)
+  Ypred_trace = NULL
+  if(keep_Y_trace){
+    if(is.null(which_genes)) which_genes = 1:G
+    if(is.null(which_samples)) which_samples = 1:N
+    Ypred_trace = matrix(0, length(which_genes)*length(which_samples), max_iter - burnin)
+  }
   
-  # gamma = rep(0.1, G)
+  a0 = 0.01
+  b0 = 0.01
+  # initialise precision parameters
   rho = 0.01
-  lambda = 10
+  lambda = 1
+  gamma = rep(1, G)
   # initialise Z
   Z = matrix(0, G, K)
   for(k in 1:K){
@@ -42,6 +52,9 @@ infer_latent_factors = function(Y, X, k_latent, gamma, alpha, max_iter = 100, bu
     noise = t(rmvnorm(N, rep(0, K), sigma_W))
     W = mu_W + noise
     
+    # Ypred
+    Ypred0 = Z %*% W
+    
     # update beta
     x_colsums = colSums(X)
     sigma_beta = 1 / (rho + sum(lambda * x_colsums**2))
@@ -49,18 +62,30 @@ infer_latent_factors = function(Y, X, k_latent, gamma, alpha, max_iter = 100, bu
     beta = mu_beta + rnorm(K, 0, sqrt(sigma_beta))
     
     # update precision parameters
+    a_lambda = a0 + 0.5*K*N
+    b_lambda = b0 + 0.5*sum((W - outer(beta, x_colsums))**2)
+    lambda = rgamma(1, a_lambda, b_lambda)
+    a_gamma = a0 + 0.5*N
+    b_gamma = b0 + 0.5*rowSums((Y - Ypred0)**2)
+    gamma = rgamma(G, a_gamma, b_gamma)
     
     # loglikelihood
     if(i > burnin){
       m_k = colSums(Z)
       loglik_Z = sum(log(alpha/K) + lgamma(m_k + alpha/K) + lgamma(G - m_k + 1) - lgamma(N + 1 + alpha/K))
-      loglik_W = sum(dnorm(W, outer(beta, colSums(X)), 1/sqrt(lambda), log=TRUE))
-      loglik_Y = sum(dnorm(Y, Z%*%W, 1/sqrt(gamma), log=TRUE))
+      loglik_W = sum(dnorm(W, outer(beta, x_colsums), 1/sqrt(lambda), log=TRUE))
+      loglik_Y = sum(dnorm(Y, Ypred0, 1/sqrt(gamma), log=TRUE))
       loglik[i-burnin] = loglik_Z + loglik_W + loglik_Y
     }
     
     # prediction
-    if(i > burnin) Ypred = Ypred + 1/(max_iter-burnin) * Z %*% W
+    if(i > burnin){
+      Ypred_mean = Ypred_mean + 1/(max_iter-burnin) * Ypred0
+      if(keep_Y_trace) Ypred_trace[, i-burnin] = melt(Ypred0[which_genes, which_samples, drop=FALSE])$value
+      # cat(sprintf("range gamma (%1.3f, %1.3f), mean(gamma): %1.3f, lambda: %1.3f\n", min(gamma), max(gamma), mean(gamma), lambda))
+    }
   }
-  return(list(Z = Z, W = W, beta = beta, Ypred = Ypred, loglik = loglik))
+  if(keep_Y_trace) Ypred_trace = postprocess_Y_trace(Y, Ypred_trace, which_genes, which_samples)
+  
+  return(list(Z = Z, W = W, beta = beta, Ypred = Ypred_mean, Ypred_trace = Ypred_trace, loglik = loglik))
 }
